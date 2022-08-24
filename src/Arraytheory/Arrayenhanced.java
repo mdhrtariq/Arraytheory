8db295d231166549235e1e076c6e5ffbd1284eab
mdhrtariq <110968471+mdhrtariq@users.noreply.github.com>
2022-08-24 12:07:39 +0530

    Arraytheory

---
+++ src/Arraytheory/Arrayenhanced.java
@@ -0,0 +1,34 @@
+package Arraytheory;
+public class Arrayenhanced {
+    public class Main {
+        public static void main(String[] args) {
+            int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int b[] = {2, 4, 6, 8, 10,};
+            int c[] = {1, 3, 5, 7, 9, 11};
+            int e[] = {1, 4, 9, 16, 25, 36, 49};
+
+            int d[][] = {a, b, c, e};
+            for (int i = 0; i < d.length; i++) {
+                for (int j = 0; j < d[i].length; j++) {
+                    System.out.print(" " + d[i][j]);
+                }
+                {
+                    System.out.println();
+                }
+            }
+            for (int k[] : d)
+            {
+                for (int l : k) {
+                    System.out.print(" " + l);
+                }
+                {
+                    System.out.println();
+                }
+            }
+
+        }
+    }
+
+
+
+}
